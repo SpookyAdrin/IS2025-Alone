b096a5c7a6a0e0e19142d667cbb426a971a47ebd
root <root@localhost.localdomain>
2025-10-15 23:54:30 +0200

    Despliegue en contenedores

--- P2-2/amigos/instance/config.py
+++ P2-2/amigos/instance/config.py
@@ -1,2 +1,4 @@
 # Definición de secretos a usar por la app
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://amigosuser:amigospass@172.18.0.2/amigosdb"
+import os
+
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite://")
